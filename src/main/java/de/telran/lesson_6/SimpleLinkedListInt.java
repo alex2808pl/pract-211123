e17c4d8a09d18bf564903de9754c80d8465187af
Aleksandr Plakushchyi <alex2808pl@gmail.com>
2024-03-13 15:43:31 +0200

    lesson_6  LinkedListIntSingly - 2

--- src/main/java/de/telran/lesson_6/SimpleLinkedListInt.java
+++ src/main/java/de/telran/lesson_6/SimpleLinkedListInt.java
@@ -8,7 +8,25 @@ public class SimpleLinkedListInt {
         list.add(9);
         list.add(1);
         list.add(4);
-
         list.show();
+
+//        list.addFirst(11);
+//        list.show();
+//        list.delete(9); // удаление из середины
+//        list.show();
+//        list.delete(11); // удаление с головы
+//        list.show();
+//        list.delete(4); // удаление последнего
+//        list.show();
+
+        list.insert(2,55);
+        list.show();
+
+//        LinkedListIntSingly list2 = new LinkedListIntSingly();
+//        list2.addFirst(11);
+//        list2.show();
+//
+//        LinkedListIntSingly list3 = new LinkedListIntSingly();
+//        list3.show();
     }
 }
