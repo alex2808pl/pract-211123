a1c824d59bb6e3b037995c35c69d51f12803e59d
shemchuk <shemchukaga@gmail.com>
2024-05-06 14:13:27 +0200

    lesson 18 short 18

--- src/main/java/de/telran/lesson_18_20240506/calc/Calcshort.java
+++ src/main/java/de/telran/lesson_18_20240506/calc/Calcshort.java
@@ -1,6 +1,7 @@
 package de.telran.lesson_18_20240506.calc;
 
-public class Calcshort implements NumericalOperations<Short>{
+
+public class Calcshort implements NumericalOperations<Short> {
 
     @Override
     public Short add(Short a, Short b) {
@@ -19,6 +20,10 @@ public class Calcshort implements NumericalOperations<Short>{
 
     @Override
     public Short div(Short a, Short b) {
+        if (b == 0) {
+            throw new ArithmeticException("Деление на ноль не допускается.");
+        }
         return (short) (a / b);
     }
+
 }
