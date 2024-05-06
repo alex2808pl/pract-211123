ce0cbd2241703fe42cc9fbe37df384403936e972
Yevgeniia Fartushko <yevgeshkaf@gmail.com>
2024-05-06 13:59:34 +0200

    Add CalculatorDouble

--- src/main/java/de/telran/lesson_18_20240506/calc/SimpleCalc.java
+++ src/main/java/de/telran/lesson_18_20240506/calc/SimpleCalc.java
@@ -7,5 +7,13 @@ public class SimpleCalc {
         System.out.println(calc.sub(10, 16));
         System.out.println(calc.multi(10, 16));
         System.out.println(calc.div(10, 16));
+
+
+
+        NumericalOperations<Double> calculator3 = new CalcDouble();
+        System.out.println(calculator3.add(10.5, 15.6));
+        System.out.println(calculator3.sub(10.5, 15.6));
+        System.out.println(calculator3.multi(10.5, 15.6));
+        System.out.println(calculator3.div(10.5, 15.6));
     }
 }
