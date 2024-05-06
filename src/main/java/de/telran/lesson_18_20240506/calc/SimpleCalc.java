85d38f0a4f42210249f8cac928f21dc8626643d4
Aleksandr Plakushchyi <alex2808pl@gmail.com>
2024-05-06 14:43:31 +0300

    lesson_18 Generic Interface

---
+++ src/main/java/de/telran/lesson_18_20240506/calc/SimpleCalc.java
@@ -0,0 +1,11 @@
+package de.telran.lesson_18_20240506.calc;
+
+public class SimpleCalc {
+    public static void main(String[] args) {
+        NumericalOperations<Integer> calc = new CalcInteger();
+        System.out.println(calc.add(10, 16));
+        System.out.println(calc.sub(10, 16));
+        System.out.println(calc.multi(10, 16));
+        System.out.println(calc.div(10, 16));
+    }
+}
