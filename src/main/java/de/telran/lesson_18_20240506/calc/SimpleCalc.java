616f98d889c2f942802bba392da777293f8b35d2
VoytovychM <mary.voytovych@gmail.com>
2024-05-06 14:12:30 +0200

    updateLong
    updateLongwithMain

--- src/main/java/de/telran/lesson_18_20240506/calc/SimpleCalc.java
+++ src/main/java/de/telran/lesson_18_20240506/calc/SimpleCalc.java
@@ -7,5 +7,12 @@ public class SimpleCalc {
         System.out.println(calc.sub(10, 16));
         System.out.println(calc.multi(10, 16));
         System.out.println(calc.div(10, 16));
+
+        NumericialOperations<Long> calc2 = new CalcLong();
+        System.out.println(calc2.add(12L, 2L ));
+        System.out.println(calc2.sub(10L, 16L));
+        System.out.println(calc2.multi(10L, 16L));
+        System.out.println(calc2.div(10L, 16L));
+
     }
 }
