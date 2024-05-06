d7dc8270f7c0d95c029a3b9462c2c9c2e9f40266
Iryna Kosiakovska <ira.oleinick2013@gmail.com>
2024-05-06 14:16:21 +0200

    updated

--- src/main/java/de/telran/lesson_18_20240506/calc/SimpleCalc.java
+++ src/main/java/de/telran/lesson_18_20240506/calc/SimpleCalc.java
@@ -8,12 +8,18 @@ public class SimpleCalc {
         System.out.println(calc.multi(10, 16));
         System.out.println(calc.div(10, 16));
 
-
-
         NumericalOperations<Double> calculator3 = new CalcDouble();
         System.out.println(calculator3.add(10.5, 15.6));
         System.out.println(calculator3.sub(10.5, 15.6));
         System.out.println(calculator3.multi(10.5, 15.6));
         System.out.println(calculator3.div(10.5, 15.6));
+        System.out.println("-------------");
+
+        NumericalOperations<Float> calc2 = new CalcFloat();
+        System.out.println(calc2.add(100.99F, 150.48F));
+        System.out.println(calc2.sub(100.99F, 150.48F));
+        System.out.println(calc2.multi(100.99F, 150.48F));
+        System.out.println(calc2.div(100.99F, 150.48F));
+
     }
 }
